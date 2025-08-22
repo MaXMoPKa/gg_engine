65af6b2b16dec82d259b10e715cfff1776adfaf3
Vladislav Aleynikov <vladislav_v_01@mail.ru>
2025-08-22 21:50:24 +0300

    Add icon for GG Studio and add Exception classes for error handling

---
+++ programs/gg_studio/src/include/exception.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <exception>
+#include <string>
+
+namespace  gg
+{
+
+class Exception : public std::exception
+{
+public:
+    Exception(int line, const char* file) noexcept;
+
+    const char* what() const noexcept override;
+    virtual const char* getType() const noexcept;
+    int getLine() const noexcept;
+    const std::string& getFile() const noexcept;
+    std::string getOriginString() const noexcept;
+
+private:
+    int line;
+    std::string file;
+
+protected:
+    mutable std::string what_buffer;
+};
+
+}
--- programs/gg_studio/src/private/main.cpp
+++ programs/gg_studio/src/private/main.cpp
@@ -1,9 +1,12 @@
+#include "src/include/exception.hpp"
 #include "src/include/window.h"
 #include <minwindef.h>
 #include <winuser.h>
 
 int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+    try
+    {
     gg::Window wnd(800, 300, "GG Studio");
 
     MSG msg;
@@ -20,6 +23,20 @@ int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLi
     }
 
     return msg.wParam;
+    }
+    catch(const gg::Exception& e)
+    {
+        MessageBox(nullptr, e.what(), e.getType(), MB_OK | MB_ICONEXCLAMATION);
+    }
+    catch(const std::exception& e)
+    {
+        MessageBox(nullptr, e.what(), "Standart Exception", MB_OK | MB_ICONEXCLAMATION);
+    }
+    catch(...)
+    {
+        MessageBox(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+    }
+    return -1;
 }
 
 int main()
